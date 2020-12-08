9b40a1b8681e4ab97b81f7e200c1030beaacb5d9
zhangyanling77 <18783226594@163.com>
2020-12-08 15:19:52 +0800

    Update index.js

--- practice/render/index.js
+++ practice/render/index.js
@@ -53,7 +53,94 @@ aHref.onclick = function(e) {
 }
 */
 
+/*
 const btn = document.querySelector('#btn');
 btn.onclick = function(e) {
   window.open('https://www.electronjs.org');
 }
+*/
+
+/*
+const { dialog } = require('electron').remote;
+const btn = document.querySelector('#btn');
+
+btn.onclick = function() {
+  dialog.showOpenDialog({
+    title: 'electron',
+    // defaultPath: 'header.jpg',
+    // 文件选择过滤
+    filters: [
+      {
+        name: 'jpg',
+        extensions: ['jpg'],
+      }
+    ],
+    buttonLabel: '确认与否',
+    // 对话框使用的功能,允许选择文件/允许多选
+    properties: ['openFile', 'multiSelections']
+  }).then(res => {
+    console.log(res);
+  }).catch(err => {
+    console.log(err);
+  });
+}
+*/
+
+/*
+const { dialog } = require('electron').remote;
+const fs = require('fs');
+const btn = document.querySelector('#btn');
+
+btn.onclick = function() {
+  dialog.showSaveDialog({
+    title: 'electron',
+  }).then(res => {
+    console.log(res);
+    fs.writeFileSync(res.filePath, 'hello world')
+  }).catch(err => {
+    console.log(err);
+  })
+}
+*/
+
+/*
+const { dialog } = require('electron').remote;
+const btn = document.querySelector('#btn');
+
+btn.onclick = function() {
+  dialog.showMessageBox({
+    type: 'error',
+    title: '错误',
+    message: '这是错误警告',
+    buttons: ['确定', '忽略', '退出']
+  }).then(res => {
+    console.log(res)
+  });
+}
+*/
+
+/*
+window.addEventListener('online', () => {
+  alert('网络正常,放心使用')
+})
+
+window.addEventListener('offline', () => {
+  alert('网络异常,请检查网络是否连接')
+})
+*/
+
+
+const { clipboard } = require('electron');
+const btn = document.getElementById('btn');
+const txt = document.getElementById('txt');
+const message = document.getElementById('message');
+
+btn.onclick = () => {
+  // 文字复制粘贴
+  clipboard.writeText(message.innerHTML, 'msg');
+  alert('复制成功！');
+  txt.innerText = clipboard.readText('msg');
+  // html添加
+  // clipboard.writeHTML('<h1>hello world</h1>');
+  // txt.innerHTML = clipboard.readHTML();
+} 
