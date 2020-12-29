7afa48b0811f09cbd0ed4de2e93ac6f6bc51786e
zhangyanling77 <18783226594@163.com>
2020-12-29 16:03:00 +0800

    Update index.js

--- react-electron/app/main/index.js
+++ react-electron/app/main/index.js
@@ -1,8 +1,10 @@
 const { app } = require('electron');
-const { create: createMainWindow } = require('./windows/main'); // 创建一个主窗口
+// const { create: createMainWindow } = require('./windows/main'); // 创建一个主窗口
 const handleIPC = require('./ipc'); // 处理主进程的事务
+const { create: createControlWindow } = require('./windows/control');
 
 app.on('ready', () => {
   handleIPC();
-  createMainWindow(); // 主窗口渲染App.js的页面内容
+  // createMainWindow(); // 主窗口渲染App.js的页面内容
+  createControlWindow();
 })
